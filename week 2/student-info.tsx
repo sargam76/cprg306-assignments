07c1c19cd142aa6ece64543ddc7b3a84bd4b2ab2
sargam76 <Sargam.Kunwor@edu.sait.ca>
2026-01-20 15:48:37 -0700

    assignment

---
+++ week 2/student-info.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+export default function StudentInfo() {
+    return (
+        <main>
+            <h1>Student Information</h1>
+            <Link href = "https://github.com/sargam76/cprg306-assignments" target="_blank" rel="noopener noreferrer">
+                <button type="button">github link</button>
+            </Link>
+        </main>
+    );
+}
