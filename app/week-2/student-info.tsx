0d2e648be965251806bb3a1d559b23335d813668
Sargam Kunwor <Sargam.Kunwor@edu.sait.ca>
2026-01-20 22:39:07 -0700

    Update student name in StudentInfo component

--- app/week-2/student-info.tsx
+++ app/week-2/student-info.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 export default function StudentInfo() {
     return (
         <main>
-            <h1>Student Information</h1>
+            <h1>Sargam Kunwwor</h1>
             <Link href = "https://github.com/sargam76/cprg306-assignments" target="_blank" rel="noopener noreferrer">
                 <button type="button">github link</button>
             </Link>
