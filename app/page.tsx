96a17be22ff1db07cc0e8b1267ed8d399e825e28
sargam76 <Sargam.Kunwor@edu.sait.ca>
2026-01-27 14:46:33 -0700

    week 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link className="p-10 m-5 bg-gray-600 text-white rounded-lg" href="/week-2"> 
-      <button type="button">week-2</button></Link>
-      <br />
-      <Link href="/week-3">
-      <button className="p-10 bg-gray-600 text-white rounded-lg" type="button">week-3</button></Link>
-      </main>
+        <h1>CPRG 306: Web Development 2 - Assignments</h1>
+        <Link href="/week-2">
+        <br />
+        <button className="p-10 m-5 bg-red-400 text-white rounded-lg" type="button">week-2</button></Link>
+        <Link href="/week-3">
+        <button className="p-10 m-5 bg-red-400 text-white rounded-lg" type="button">week-3</button></Link>
+    </main>
   );
 }
