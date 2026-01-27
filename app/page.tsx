c7d948eb6c72bc2c6b3075e4817b2d4c42ead06a
sargam76 <Sargam.Kunwor@edu.sait.ca>
2026-01-27 14:37:49 -0700

    week 3

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,11 @@ export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">Go to Week 2</Link>
-    </main>
+      <Link className="p-10 m-5 bg-gray-600 text-white rounded-lg" href="/week-2"> 
+      <button type="button">week-2</button></Link>
+      <br />
+      <Link className="p-10 m-5 bg-gray-600 text-white rounded-lg" href="/week-3">
+      <button className="p-10 bg-gray-600 text-white rounded-lg" type="button">week-3</button></Link>
+      </main>
   );
 }
--- app/week-2/student-info.tsx
+++ app/week-2/student-info.tsx
@@ -2,11 +2,7 @@ import Link from "next/link";
 export default function StudentInfo() {
     return (
         <main>
-<<<<<<< HEAD
-            <h1>Sargam Kunwor</h1>
-=======
             <h1>Sargam Kunwwor</h1>
->>>>>>> 0d2e648be965251806bb3a1d559b23335d813668
             <Link href = "https://github.com/sargam76/cprg306-assignments" target="_blank" rel="noopener noreferrer">
                 <button type="button">github link</button>
             </Link>
